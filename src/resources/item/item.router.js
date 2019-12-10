4173750e895fd739d4f9ac3b94ad2bfe8fe68904
Trevor Storey <trevor@photoshelter.com>
2019-12-10 16:59:52 -0700

    [lesson-2] add routes for /api/item

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -2,4 +2,23 @@ import { Router } from 'express'
 
 const router = Router()
 
+// /api/item/
+router
+  .route('/')
+  .get((req, res) => res.send({ message: 'heyo get /' }))
+  .post((req, res) => res.send({ message: 'heyo post /' }))
+
+// /api/item/:id
+router
+  .route('/:id')
+  .get((req, res) => res.send({ message: 'heyo get /:id' }))
+  .delete((req, res) => res.send({ message: 'heyo delete /:id' }))
+  .put((req, res) => res.send({ message: 'heyo put /:id' }))
+
+// router.get('/', (req, res) => res.send({ message: 'heyo get' }))
+// router.post('/', (req, res) => res.send({ message: 'heyo post' }))
+// router.get('/:id', (req, res) => res.send({ message: 'heyo get /:id' }))
+// router.put('/:id', (req, res) => res.send({ message: 'heyo put /:id' }))
+// router.delete('/:id', (req, res) => res.send({ message: 'heyo delete /:id' }))
+
 export default router
