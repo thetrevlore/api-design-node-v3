363981c59369d2569dcda2486cf6a13fcc60034e
Trevor Storey <trevor@photoshelter.com>
2019-12-10 15:10:35 -0700

    [lesson-1] implemented a few controllers

--- src/server.js
+++ src/server.js
@@ -12,4 +12,25 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+app.get('/', (req, res, next) => {
+  res.send({ message: 'hello' })
+})
+
+app.post('/', (req, res, next) => {
+  console.log(req.body)
+  res.send({ message: 'ok' })
+})
+
+app.get('/data', (req, res) => {
+  res.send({ message: 'howdy' })
+})
+
+app.post('/data', (req, res) => {
+  res.send(req.body)
+})
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('server is on 3000')
+  })
+}
