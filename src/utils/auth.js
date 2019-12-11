370fe107f35653b2ffd61807299c27f712189f36
Trevor Storey <trevor@photoshelter.com>
2019-12-10 21:06:46 -0700

    [lesson-5] auth, jwt, etc

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -16,10 +16,67 @@ export const verifyToken = token =>
     })
   })
 
-export const signup = async (req, res) => {}
+export const signup = async (req, res) => {
+  // create user
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).send({ message: 'email and password required' })
+  }
 
-export const signin = async (req, res) => {}
+  const newUser = await User.create(req.body)
+  const token = newToken(newUser)
+  return res.status(201).send({ token: token })
+}
+
+export const signin = async (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).send({ message: 'email and password required' })
+  }
+
+  const foundUser = await User.findOne({ email: req.body.email })
+  // .select('email password')
+  // .exec()
+  if (!foundUser) {
+    return res.status(401).send({ message: 'user not found' })
+  }
+
+  const match = await foundUser.checkPassword(req.body.password)
+  if (!match) {
+    return res.status(401).send({ message: 'password incorrect' })
+  }
+
+  const token = newToken(foundUser)
+  return res.status(201).send({ token })
+}
 
 export const protect = async (req, res, next) => {
+  if (
+    !req.headers.authorization ||
+    !req.headers.authorization.startsWith('Bearer ')
+  ) {
+    return res.status(401).end()
+  }
+
+  const token = req.headers.authorization.split(' ')[1]
+  let payload
+  try {
+    payload = await verifyToken(token)
+  } catch (error) {
+    return res.status(401).end()
+  }
+
+  const user = await User.findById(payload.id)
+    .select('-password')
+    .lean()
+  // .exec()
+
+  if (!user) {
+    return res.status(401).end()
+  }
+
+  // verify the token
+  // look for jwt in the authorization header
+  // 'Bearer lkasdfoinasdfoijf'
+
+  req.user = user
   next()
 }
