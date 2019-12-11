ff3833b6803c1c0259be75b2accea81299b40461
Trevor Storey <trevor@photoshelter.com>
2019-12-10 17:45:45 -0700

    [lesson-3] define item model schema

--- src/resources/item/item.model.js
+++ src/resources/item/item.model.js
@@ -1,4 +1,36 @@
 import mongoose from 'mongoose'
+var ObjectId = mongoose.Schema.Types.ObjectId
 
-const itemSchema = new mongoose.Schema({}, { timestamps: true })
+const itemSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      maxlength: 50,
+      required: true,
+      trim: true
+    },
+    status: {
+      type: String,
+      default: 'active',
+      enum: ['active', 'complete', 'pastdue'],
+      required: true
+    },
+    notes: String,
+    due: Date,
+    createdBy: {
+      type: ObjectId,
+      ref: 'user',
+      required: true
+    },
+    list: {
+      type: ObjectId,
+      ref: 'list',
+      required: true
+    }
+  },
+  { timestamps: true }
+)
+
+itemSchema.index({ list: 1, name: 1 }, { unique: true })
+
 export const Item = mongoose.model('item', itemSchema)
