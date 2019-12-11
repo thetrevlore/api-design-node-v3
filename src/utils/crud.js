b6b1f22740a0660096b56eeedf72ac1b66196cfe
Trevor Storey <trevor@photoshelter.com>
2019-12-10 19:14:36 -0700

    [lesson-4] create generic crud controllers. practice mongoose and express

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,12 +1,63 @@
-export const getOne = model => async (req, res) => {}
+export const getOne = model => async (req, res) => {
+  const doc = await model
+    .findOne({
+      _id: req.params.id,
+      createdBy: req.user._id
+    })
+    .exec()
 
-export const getMany = model => async (req, res) => {}
+  if (!doc) {
+    return res.status(404).end()
+  }
 
-export const createOne = model => async (req, res) => {}
+  res.status(200).json({ data: doc })
+}
 
-export const updateOne = model => async (req, res) => {}
+export const getMany = model => async (req, res) => {
+  const docs = await model.find({ createdBy: req.user._id })
 
-export const removeOne = model => async (req, res) => {}
+  res.status(200).json({ data: docs })
+}
+
+export const createOne = model => async (req, res) => {
+  const doc = await model.create({
+    // name: req.body.name
+    ...req.body,
+    createdBy: req.user._id
+  })
+
+  res.status(201).json({ data: doc })
+}
+
+export const updateOne = model => async (req, res) => {
+  const updatedDoc = await model.findOneAndUpdate(
+    {
+      createdBy: req.user._id,
+      _id: req.params.id
+    },
+    req.body,
+    { new: true } // gets the updated document back rather than the old one
+  )
+
+  if (!updatedDoc) {
+    return res.status(400).end()
+  }
+
+  res.status(200).json({ data: updatedDoc })
+}
+
+export const removeOne = model => async (req, res) => {
+  const removedDoc = await model.findOneAndRemove({
+    createdBy: req.user._id,
+    _id: req.params.id
+  })
+
+  if (!removedDoc) {
+    return res.status(400).end()
+  }
+
+  res.status(200).json({ data: removedDoc })
+}
 
 export const crudControllers = model => ({
   removeOne: removeOne(model),
